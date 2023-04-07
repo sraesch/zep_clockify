57ec38af1081aff18558835f88fe967346d3e9c6
Sascha Räsch <sascha.raesch@gmail.com>
2023-04-07 15:56:35 +0200

    changed: removed CSV crate dependency
    Removed CSV dependency as it could not handle multi-line columns.

--- src/projects_loader.rs
+++ src/projects_loader.rs
@@ -1,6 +1,8 @@
 use std::{path::Path, fs::File};
 use anyhow::Result;
 
+use crate::csv_parser::{CSVParser, CSVReader};
+
 pub struct Project {
     name: String,
     description: String,
@@ -10,14 +12,35 @@ impl Project {
 
 }
 
+struct ProjectsReader {
+    projects: Vec<Project>,
+}
+
+impl ProjectsReader {
+    pub fn new() -> Self {
+        Self{
+            projects: Vec::new(),
+        }
+    }
+}
+
+impl CSVReader for ProjectsReader {
+    fn header_record(&mut self, header: Vec<String>) {
+        todo!()
+    }
+
+    fn record(&mut self, record: &[String]) {
+        todo!()
+    }
+}
+
 pub fn load_projects(file_name: &Path) -> Result<Vec<Project>> {
     let mut file = File::open(file_name)?;
-    let mut rdr = csv::Reader::from_reader(file);
+    let mut rdr = CSVParser::new(file);
 
-    for result in rdr.records() {
-        let record = result?;
-        println!("{:?}", record);
-    }
+    let mut project_reader = ProjectsReader::new();
+
+    rdr.read(&mut project_reader)?;
 
     todo!()
 
